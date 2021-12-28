f6b11172fedc24f0d5480729e13539241b3e22fb
go367it <satyamkumar3001@gmail.com>
2021-12-28 22:33:35 +0530

    added home page

---
+++ pages/documentation/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Documentation = () => {
+    return (
+        <div>
+            hcgjvhkb
+        </div>
+    )
+}
+
+export default Documentation
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      
+    <div>
+      <div className="w-full h-full">
+        <div className="flex justify-center items-center">
+          <div>
+            <h1 className="text-2xl text-center py-4">Object Ui</h1>
+            <Link href="/documentation">
+              <button className="bg-blue-500 py-3 px-4 text-white">
+                Documentation
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
